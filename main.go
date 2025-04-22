478ca1e33b04c0b6d4514efba06dd378e5a6da1c
Vlas Nagibin <vlasn@rabbit.co.th>
2025-04-22 14:24:58 +0700

    style(main): provide main() and doSmth() with comments

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	// concrete implementations
 	rlog := rlogging.New()
 	slog := slogging.New()
 
@@ -18,5 +19,10 @@ func main() {
 
 func doSmth(log logging.Logger) {
 	var err = fmt.Errorf("test error %s", "test")
+
+	// Since the variable log is of interface type,
+	// only the methods defined in the interface are accessible.
+	// Therefore, the interface must include all the methods
+	// that are used across our services.
 	log.WithError(err).Error("test error")
 }
