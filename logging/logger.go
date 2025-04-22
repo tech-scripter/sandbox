c8b2981b68c04ec4cb8f009fce217aebee62925e
Vlas Nagibin <vlasn@rabbit.co.th>
2025-04-22 14:25:23 +0700

    style(main): delete comment for Logger interface

--- logging/logger.go
+++ logging/logger.go
@@ -1,8 +1,5 @@
 package logging
 
-// Logger is an interface that wraps the basic Printf method.
-// This interface provides a unified way to use loggers from various third-party libraries as function parameters.
-// For instance, the SetTraceLog() in github.com/olivere/elastic/v7 accepts a logger of type Logger.
 type Logger interface {
 	Printf(format string, args ...interface{})
 	WithError(err error) Logger
